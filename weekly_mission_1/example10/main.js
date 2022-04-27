3e0acff684864e1fdae7194a5ab68f00f8bac1cf
Luis Rodriguez <jl.rodriguezelias@ugto.mx>
2022-04-27 17:29:16 -0500

    Corrección de conceptos para exportar example10

--- weekly_mission_1/example10/main.js
+++ weekly_mission_1/example10/main.js
@@ -1,4 +1,20 @@
 import MyPokemon from './pokemon.js'
 
-const pikachu = new MyPokemon('Pikachu')
-pikachu.sayHello()
+
+const pikachu = new MyPokemon('Pikachu');
+const bulbasaur = new MyPokemon("bulbasaur");
+const squirtle = new MyPokemon("squirtle");
+const charmander = new MyPokemon("charmander");
+
+pikachu.sayHello();
+pikachu.sayMessage("Hey!!");
+
+bulbasaur.sayHello();
+bulbasaur.sayMessage("Heey!");
+
+charmander.sayHello();
+charmander.sayMessage("Heey!");
+
+squirtle.sayHello();
+squirtle.sayMessage("Heey!");
+
---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,19 @@
+//Crea un archivo pokemon.js, ahí crea la clase que necesitas para correr el archivo main.js. Deberás exportarla usando ESM.
+//Necesitarás usar export default.
+//La clase tiene la misma implementación que el ejercicio anterior.
+//Finalmente corre: node main.js
+
+export default class Pokemon {
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHello(){
+        console.log(`Mi Pokemon ${this.name} te saluda`);
+    }
+
+    sayMessage(message){
+        console.log(`${message}`)
+    }
+}
+

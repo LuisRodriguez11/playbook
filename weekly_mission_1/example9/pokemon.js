8d75ca0d8c9fc710855ec467fb029ebcec606b52
Luis Rodriguez <jl.rodriguezelias@ugto.mx>
2022-04-27 16:18:29 -0500

    Agregando comentarios example10

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -1,5 +1,8 @@
 class Pokemon {
     constructor(name){ //todos los parametros que reciba constructor es una instancia que se le pasa a la clase
+        
+    // this.name es la variable que se guarda en el contexto local
+    // message es una variable que se le pasa al ejecutar este método
         this.name = name //contexto de la clase (propiedades de la clase pokemon)
         
     
@@ -14,4 +17,5 @@ class Pokemon {
     }
 }
 
+//Esta clase exporta este modulo
 module.exports = Pokemon

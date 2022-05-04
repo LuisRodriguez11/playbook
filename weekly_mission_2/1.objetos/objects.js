40e192a6f65f8b626ba36d5c3e473db99e1f76fc
Luis Rodriguez <jl.rodriguezelias@ugto.mx>
2022-05-03 23:57:04 -0500

    Agregando 3.Crear un objeto con propiedades diferentes

--- weekly_mission_2/1.objetos/objects.js
+++ weekly_mission_2/1.objetos/objects.js
@@ -11,6 +11,27 @@ const myObject2 = {
 }
 console.log("Ejemplo 2: Creando un objeto con propiedades");
 console.log(myObject2);
+//3.Creando un objeto con propiedades diferentes
+const myobject3 = {
+    name: "Benito",
+    age: 5,
+    nicknames: [
+        "Beno",
+        "Gordo",
+    ],
+
+    adress: {
+        zip_code: 11291112,
+        street: "1ra Priv. Virrey Buccareli",
+        city: "Morelia"
+    }
+}
+
+console.log("Ejemplo 3: Creando un objeto con diferentes propiedades");
+console.log(myobject3);
+console.log(myobject3.name);
+console.log(myobject3.adress);
+console.log(myobject3["adress"]);
 
 
 
